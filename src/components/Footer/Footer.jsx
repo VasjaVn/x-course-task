f09dbdbae1d41d6232868233fb9f3f2460db9c08
My <my@email>
2023-03-03 20:43:02 +0200

    change footer text

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="footer-container">
             <p>
-                Виконано в <a href="https://prometheus.org.ua/" target="_blank" rel="noopener noreferrer">Prometheus</a> © 2022
+                Виконано by <a href="https://prometheus.org.ua/" target="_blank" rel="noopener noreferrer">Vasja Vn</a> © 2022
             </p>
         </footer>
     );
